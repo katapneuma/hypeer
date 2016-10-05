23908b4e6f9f0b873f0e4078eae42924b6664cad
tarzier <lcagas@o.com>
2016-10-05 18:16:25 +0800

    Initial changes when wrapping in Template10

---
+++ hypeer/hypeer/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using hypeer.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace hypeer.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
